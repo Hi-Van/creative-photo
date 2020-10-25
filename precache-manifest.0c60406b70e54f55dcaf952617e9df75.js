82ea19ff4e7ed7cc6ede95d00e04025b802bc1ac
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-10-25 17:28:35 -0400

    Updates

---
+++ precache-manifest.0c60406b70e54f55dcaf952617e9df75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aad401e6f432068546ea156e76a3333",
+    "url": "/phoooto/index.html"
+  },
+  {
+    "revision": "a56b8be95067c92f9e28",
+    "url": "/phoooto/static/css/main.54a045fa.chunk.css"
+  },
+  {
+    "revision": "9ee2105516aca9e18797",
+    "url": "/phoooto/static/js/2.933fee92.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/phoooto/static/js/2.933fee92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a56b8be95067c92f9e28",
+    "url": "/phoooto/static/js/main.213a5124.chunk.js"
+  },
+  {
+    "revision": "a5532d0f937101f5b1cd",
+    "url": "/phoooto/static/js/runtime-main.d072cc4e.js"
+  },
+  {
+    "revision": "38d0cdd3f10ccec5a5a72df7b3e9765e",
+    "url": "/phoooto/static/media/screen-capture-creative.38d0cdd3.jpg"
+  }
+]);
